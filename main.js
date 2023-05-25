6b831147fe7c0c9df763303db7b66eb844056474
j0srisk <j0srisk@gmail.com>
2023-05-25 17:01:53 -0500

    refreshed start screen styling

--- main.js
+++ main.js
@@ -4,36 +4,8 @@ import gameUI from './src/gameUI'
 
 let game = gameController();
 
-const app = document.querySelector('#app')
-app.innerHTML = '';
-
-const gameTitle = document.createElement('h1');
-gameTitle.classList.add('game-title');
-gameTitle.textContent = 'Battleship';
-app.appendChild(gameTitle);
-
-const userInputContainer = document.createElement('div');
-userInputContainer.classList.add('user-input-container');
-app.appendChild(userInputContainer);
-
-const userNameLabel = document.createElement('label');
-userNameLabel.classList.add('user-name-label');
-userNameLabel.textContent = 'Player Name: ';
-userInputContainer.appendChild(userNameLabel);
-
-const userNameInputContainer = document.createElement('div');
-userNameInputContainer.classList.add('user-name-input-container');
-userInputContainer.appendChild(userNameInputContainer);
-
-const userNameInput = document.createElement('input');
-userNameInput.classList.add('user-name-input');
-userNameInput.value = 'Player 1';
-userNameInputContainer.appendChild(userNameInput);
-
-const startButton = document.createElement('button');
-startButton.classList.add('start-button');
-startButton.textContent = 'Start';
-userNameInputContainer.appendChild(startButton);
+const startButton = document.querySelector('.user-name-submit');
+const userNameInput = document.querySelector('.user-name-input');
 
 startButton.onclick = function() {
   game.user.name = userNameInput.value;
