9696264b79e4ad255f1cee4c91026b33774e9d16
j0srisk <j0srisk@gmail.com>
2023-05-27 17:50:44 -0500

    updated tests

--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -1,39 +1,59 @@
-import { assert, beforeEach, describe, expect, it } from 'vitest'
-import gameboardFactory from '../factories/gameboard'
-import shipFactory from '../factories/ship'
+import {
+  assert, beforeEach, describe, expect, it,
+} from 'vitest';
+import gameboardFactory from '../factories/gameboard';
+import shipFactory from '../factories/ship';
 
 describe('gameboard functions', () => {
+  let testGameboard;
 
-    let testGameboard;
+  beforeEach(() => {
+    testGameboard = gameboardFactory();
+  });
 
-    beforeEach(() => {
-        testGameboard = gameboardFactory();
-    });
+  it('placeShip function horizontal', () => {
+    const testShip = shipFactory(4);
+    testGameboard.placeShip(testShip, 0, 'horizontal');
+    assert.equal(testGameboard.board[0], testShip);
+    assert.equal(testGameboard.board[1], testShip);
+    assert.equal(testGameboard.board[2], testShip);
+    assert.equal(testGameboard.board[3], testShip);
+  });
 
-    it('placeShip function', () => {
-        const testShip = shipFactory(4);
-        testGameboard.placeShip(testShip, 0, 'horizontal');
-        assert.equal(testGameboard.board[0], testShip);
-        assert.equal(testGameboard.board[1], testShip);
-        assert.equal(testGameboard.board[2], testShip);
-        assert.equal(testGameboard.board[3], testShip);
-    });
+  it('placeShip function vertical', () => {
+    const testShip = shipFactory(4);
+    testGameboard.placeShip(testShip, 0, 'vertical');
+    assert.equal(testGameboard.board[0], testShip);
+    assert.equal(testGameboard.board[10], testShip);
+    assert.equal(testGameboard.board[20], testShip);
+    assert.equal(testGameboard.board[30], testShip);
+  });
 
-    it('receiveAttack function', () => {
-        const testShip = shipFactory(4);
-        testGameboard.placeShip(testShip, 0, 'horizontal');
-        testGameboard.receiveAttack(0);
-        assert.equal(testGameboard.board[0], testShip);
-        assert.equal(testGameboard.board[0].hit[0], 0);
-    });
+  it('receiveAttack function', () => {
+    const testShip = shipFactory(4);
+    testGameboard.placeShip(testShip, 0, 'horizontal');
+    testGameboard.receiveAttack(0);
+    assert.equal(testGameboard.board[0], testShip);
+    assert.equal(testGameboard.board[0].hit[0], 0);
+  });
 
-    it('allSunk function', () => {
-        const testShip = shipFactory(4);
-        testGameboard.placeShip(testShip, 0, 'horizontal');
-        testGameboard.receiveAttack(0);
-        testGameboard.receiveAttack(1);
-        testGameboard.receiveAttack(2);
-        testGameboard.receiveAttack(3);
-        assert.equal(testGameboard.allSunk(), true);
-    });
+  it('allSunk function horizontal', () => {
+    const testShip = shipFactory(4);
+    testGameboard.placeShip(testShip, 0, 'horizontal');
+    testGameboard.receiveAttack(0);
+    testGameboard.receiveAttack(1);
+    testGameboard.receiveAttack(2);
+    testGameboard.receiveAttack(3);
+    assert.equal(testGameboard.allSunk(), true);
+  });
+
+  it('allSunk function vertical', () => {
+    const testShip = shipFactory(4);
+    testGameboard.placeShip(testShip, 0, 'vertical');
+    testGameboard.receiveAttack(0);
+    testGameboard.receiveAttack(10);
+    testGameboard.receiveAttack(20);
+    testGameboard.receiveAttack(30);
+    assert.equal(testGameboard.allSunk(), true);
+  });
 });
--- src/tests/ship.test.js
+++ src/tests/ship.test.js
@@ -1,28 +1,33 @@
-import { assert, beforeEach, describe, expect, it } from 'vitest'
-import shipFactory from '../factories/ship'
+import {
+  assert, beforeEach, describe, expect, it,
+} from 'vitest';
+import shipFactory from '../factories/ship';
 
 describe('ship functions', () => {
-    
-    let testShip;
+  let testShip;
 
-    beforeEach(() => {
-        testShip = shipFactory(4);
-    });
+  beforeEach(() => {
+    testShip = shipFactory(4, 'Battleship');
+  });
 
-    it('ship length', () => {
-        assert.equal(testShip.length, 4);
-    });
+  it('ship length', () => {
+    assert.equal(testShip.length, 4);
+  });
 
-    it('hit function', () => {
-        testShip.hitShip(0);
-        assert.equal(testShip.hit[0], 0);
-    });
+  it('ship name', () => {
+    assert.equal(testShip.name, 'Battleship');
+  });
 
-    it('isSunk function', () => {
-        testShip.hitShip(0);
-        testShip.hitShip(1);
-        testShip.hitShip(2);
-        testShip.hitShip(3);
-        assert.equal(testShip.isSunk(), true);
-    });
+  it('hit function', () => {
+    testShip.hitShip(0);
+    assert.equal(testShip.hit[0], 0);
+  });
+
+  it('isSunk function', () => {
+    testShip.hitShip(0);
+    testShip.hitShip(1);
+    testShip.hitShip(2);
+    testShip.hitShip(3);
+    assert.equal(testShip.isSunk(), true);
+  });
 });
